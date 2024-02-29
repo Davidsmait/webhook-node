04d7db6ccee4cb4550e608bf06cbda1bf8b1ef98
davidsan <david.sanluis@qrsof.com>
2024-02-29 17:23:52 -0600

    sdk ngrock

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const http = require('http');
+const ngrok = require('@ngrok/ngrok');
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const PORT = 3000
+const PORT = 8000
 // Setup a webhook route
 app.use(bodyParser.json())
 app.post('/ultramsgwebhook', (req, res) => {
@@ -14,3 +17,15 @@ app.post('/ultramsgwebhook', (req, res) => {
 
 app.use(bodyParser.json())
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}🚀 `))
+
+// Create webserver
+// http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.end('Congrats you have created an ngrok web server');
+// }).listen(8080, () => console.log('Node.js web server at 8080 is running...'));
+
+// Get your endpoint online
+ngrok.connect({ addr: PORT, authtoken_from_env: true })
+    .then(listener => console.log(`Ingress established at: ${listener.url()}`));
+
+
